e0e4c615937dd58f57f7d8ea856fba7d6961b861
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-22 18:48:54 +0530

    Commit

---
+++ Welcome-Api/src/test/java/com/example/Welcome_Api/WelcomeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Welcome_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
